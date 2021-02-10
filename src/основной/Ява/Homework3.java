5cd4571e8dcd9fd89433aeacedc7941b62c8ff05
Nezhin <78836311+Nezhin@users.noreply.github.com>
2021-02-10 17:18:03 +0300

    Update and rename src/main/java/Homework3.java to src/основной/Ява/Homework3.java

--- src/основной/Ява/Homework3.java
+++ src/основной/Ява/Homework3.java
@@ -1,13 +1,12 @@
 public class Homework3 {
     public static void main(String[] args) {
         System.out.println("Задание 3");
-        float answer = equation();
+        float answer = equation(17,14.57f,18,6);
         String space = " ";
         System.out.println("Вычисляющий и возвращающий результат метод" + space + answer);
     }
 
-       public static float equation(){
-          float  a = 17f, b = 14.57f, c = 18f, d = 6f;
-          return  a * (b + (c / d));
+       public static float equation(float a, float b, float c, float d){
+        return  a * (b + (c / d));
         }
     }
