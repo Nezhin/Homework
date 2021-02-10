ce10c66058b55384ff18de6aca3abf78d2a740b6
Nezhin <78836311+Nezhin@users.noreply.github.com>
2021-02-10 17:59:59 +0300

    Update and rename src/main/java/Homework7.java to src/основной/Ява/Homework7.java

--- src/main/java/Homework7.java
+++
@@ -1,8 +0,0 @@
-public class Homework7 {
-    public static void main(String[] args) {
-        public static void Name(String name){
-            System.out.println("Привет, " + name + "!");
-        }
-    }
-}
-}
---
+++ src/основной/Ява/Homework7.java
@@ -0,0 +1,12 @@
+public class Homework7 {
+    public static void main(String[] args) {
+    String ok = Name();
+    }
+    
+    public static String Name(){
+        String name = new String();
+        System.out.println("Привет, " + name + "!");
+        return name;
+    }
+      
+}
