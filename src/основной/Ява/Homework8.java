d03903c0c172d7560920e3f7d8219329cac192fe
Nezhin <78836311+Nezhin@users.noreply.github.com>
2021-02-10 18:05:01 +0300

    Update and rename src/main/java/Homework8.java to src/основной/Ява/Homework8.java

--- src/основной/Ява/Homework8.java
+++ src/основной/Ява/Homework8.java
@@ -1,11 +1,13 @@
 public class Homework8 {
     public static void main(String[] args) {
-        int x = 2000;  //x=год
-        if(x % 4 == 0 || x % 400 == 0 || x % 100 != 0) {
+        int year = calculate(2000);
+    }
+        public static int calculate(int x){
+    if(x % 4 == 0 || x % 400 == 0 || x % 100 != 0) {
             System.out.println("Год" + x + "является високосным");
         }else{
             System.out.println("Год" + x + "не является високосным");
-
         }
+    return x;
     }
 }
