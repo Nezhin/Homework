549f330527f6778b4aa092991245d4ab9ba1822b
Nezhin <78836311+Nezhin@users.noreply.github.com>
2021-02-10 08:41:45 +0300

    Add files via upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+package Game;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ваша задача угадать число.");
+        int range = 360;
+        int number = (int)(Math.random()* range);
+        while(true){
+            System.out.println("Угадайте число от 0 до "+range);
+            int input_number = scanner.nextInt();
+            if(input_number==number){
+                System.out.println("Вы угадали.");
+                break;
+            } else if(input_number > number) {
+                System.out.println("Загаданное число меньше");
+            } else {
+                System.out.println("Загаданное число ,больше");
+            }
+        }
+        scanner.close();
+    }
+
+}
+
