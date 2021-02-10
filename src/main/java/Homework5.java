e1e998f5b071f2e24277feee21ebb549242fb2b9
Nezhin <mister.nezhin@gmail.com>
2021-02-10 08:03:50 +0300

    Написал на языке Java

---
+++ src/main/java/Homework5.java
@@ -0,0 +1,12 @@
+public class Homework5 {
+    public static void main(String[] args) {
+        String s = " ";
+        int a = 5;
+        if (a >= 0) {
+            System.out.println("Число" + s + a + s + "положительное");
+        }else{
+            System.out.println("Число" + s + a + s + "отрицательное");
+
+        }
+    }
+}
---
+++ src/main/java/Homework7.java
@@ -0,0 +1,8 @@
+public class Homework7 {
+    public static void main(String[] args) {
+        public static void Name(String name){
+            System.out.println("Привет, " + name + "!");
+        }
+    }
+}
+}
