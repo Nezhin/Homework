e967d2ebc3aa85fd0dbfc1e4a4924b94a8660541
Nezhin <78836311+Nezhin@users.noreply.github.com>
2021-03-31 14:04:04 +0300

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    TextArea textArea;
+
+    @FXML
+    TextField textField;
+
+
+    public void sendMessage() {
+        if (!textField.getText().equals("")) {
+            textArea.appendText("Я: " + textField.getText() + "\n");
+            textField.clear();
+            textField.requestFocus();
+        }
+    }
+}
