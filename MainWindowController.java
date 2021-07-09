b9b9559a806d4f54306034ecdf5b2c504b554163
Nezhin <78836311+Nezhin@users.noreply.github.com>
2021-07-09 20:55:39 +0300

    Cloud_Homework4

---
+++ MainWindowController.java
@@ -0,0 +1,37 @@
+package root.fx;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+public class MainWindowController {
+
+    @FXML
+    TextField pathField;
+
+    @FXML
+    public void initialize()  {
+
+    }
+
+    @FXML
+    private void back(){
+        // TODO
+    }
+
+    @FXML
+    private void showFolderCreateForm(){
+        // TODO
+    }
+
+    @FXML
+    private void addFile(){
+        // TODO
+    }
+
+
+
+}
